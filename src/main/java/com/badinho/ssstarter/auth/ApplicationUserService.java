f52cbeaf0292ac9324221ee601d6e15d766e26f0
BadinhoCornejo <badinhocornejo@gmail.com>
2023-07-15 09:05:12 -0500

    add ApplicationUserService qualifier to match implementation

--- src/main/java/com/badinho/ssstarter/auth/ApplicationUserService.java
+++ src/main/java/com/badinho/ssstarter/auth/ApplicationUserService.java
@@ -1,15 +1,18 @@
 package com.badinho.ssstarter.auth;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class ApplicationUserService implements UserDetailsService {
     private final ApplicationUserDao applicationUserDao;
+
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
+        this.applicationUserDao = applicationUserDao;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
